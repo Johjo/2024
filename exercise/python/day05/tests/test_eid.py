4ef864cb37d03173dab54d0a8ff29b61ecbd127a
Jonathan LAURENT <johjo@ytreza.org>
2024-12-05 23:01:39 +0100

    green

--- exercise/python/day05/tests/test_eid.py
+++ exercise/python/day05/tests/test_eid.py
@@ -2,6 +2,14 @@ import unittest
 
 import pytest
 
+#### Test list
+# - [ ] enregistrer la naissance d'un nouvel elfe
+# - [ ] identifier un elfe via son EID
+# - [ ] lister tous les elfes par nom et EID
+# - [ ] dire si un EID n'est pas valide pour un elfe
+
+
+
 
 def test_eid():
     assert "a" == "a"

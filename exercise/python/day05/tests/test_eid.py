1ba1f2510b50961605997bd196cc1a6c8aa4ed75
Jonathan LAURENT <johjo@ytreza.org>
2024-12-05 23:12:37 +0100

    green

--- exercise/python/day05/tests/test_eid.py
+++ exercise/python/day05/tests/test_eid.py
@@ -5,7 +5,7 @@ import pytest
 #### Test list
 # - [.] enregistrer la naissance d'un nouvel elfe
 # - [.] calculer à la main l'identifiant d'un premier elfe né en 1984 de sexe Sloubi
-# - [.] créer une fonction permettant de calculer le complément d'un elfe
+# - [x] créer une fonction permettant de calculer le complément d'un elfe
 # - [ ] identifier un elfe via son EID
 # - [ ] lister tous les elfes par nom et EID
 # - [ ] dire si un EID n'est pas valide pour un elfe

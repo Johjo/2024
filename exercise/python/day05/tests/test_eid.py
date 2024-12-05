ae332c1c1ca2fe9a556d205e57edb84beb5307e6
Jonathan LAURENT <johjo@ytreza.org>
2024-12-05 23:50:26 +0100

    red

--- exercise/python/day05/tests/test_eid.py
+++ exercise/python/day05/tests/test_eid.py
@@ -52,6 +52,7 @@ class Sex(Enum):
     (Sex.Sloubi, 84001, "Pipon", "18400108"),
     (Sex.Sloubi, 85001, "Pipou", "18500175"),
     (Sex.Gagna, 86001, "Pipette", "28600152"),
+    (Sex.Catact, 87001, "Pipelette", "38700100"),
 ])
 def test_register_when_elf_is_born(sex: Sex, eid_other: int, name: str, expected_eid, ):
     # GIVEN

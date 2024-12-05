64f2db1f572912392a8c812513673afa7ddb673f
Jonathan LAURENT <johjo@ytreza.org>
2024-12-05 23:57:35 +0100

    red

--- exercise/python/day05/tests/test_eid.py
+++ exercise/python/day05/tests/test_eid.py
@@ -53,10 +53,12 @@ class Sex(Enum):
 
 
 class ElfRegister:
+    def __init__(self, elves_set: ElvesSetInMemory):
+        self.elves_set = elves_set
     def execute(self, sex: Sex, year_of_birth: int,  name: str):
         eid_prefix = sex.value * 100000 + year_of_birth * 1000 + 1
         eid = f"{eid_prefix}{str(control_key(eid_prefix)).zfill(2)}"
-        elves_set.save(eid, name)
+        self.elves_set.save(eid, name)
 
 
 @pytest.mark.parametrize("sex, year_of_birth, name, expected_eid", [

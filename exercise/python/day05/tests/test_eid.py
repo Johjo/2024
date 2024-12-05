9ee19d914a5e90694aaf78ea9e526306b97ffabb
Jonathan LAURENT <johjo@ytreza.org>
2024-12-06 00:18:34 +0100

    green

--- exercise/python/day05/tests/test_eid.py
+++ exercise/python/day05/tests/test_eid.py
@@ -73,10 +73,14 @@ class ElfRegister:
         self.elves_set = elves_set
 
     def execute(self, sex: Sex, year_of_birth: int,  name: str):
+        eid = self.calculate_eid(sex, year_of_birth)
+        self.elves_set.save(eid=eid, elf=Elf(name=name, year_of_birth=year_of_birth))
+
+    def calculate_eid(self, sex, year_of_birth):
         year_count = self._count_elves_by_year(year_of_birth) + 1
         eid_prefix = sex.value * 100000 + year_of_birth * 1000 + year_count
         eid = f"{eid_prefix}{str(control_key(eid_prefix)).zfill(2)}"
-        self.elves_set.save(eid=eid, elf=Elf(name=name, year_of_birth=year_of_birth))
+        return eid
 
     def _count_elves_by_year(self, year_of_birth):
         return len([elf for elf in (self.elves_set.all()) if elf.year_of_birth == year_of_birth])

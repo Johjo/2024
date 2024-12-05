110192e09e86590d777ba6d370bb227a347a827a
Jonathan LAURENT <johjo@ytreza.org>
2024-12-05 23:50:35 +0100

    red

--- exercise/python/day05/tests/test_eid.py
+++ exercise/python/day05/tests/test_eid.py
@@ -47,6 +47,7 @@ class ElvesSetInMemory:
 class Sex(Enum):
     Sloubi = 1
     Gagna = 2
+    Catact = 3
 
 @pytest.mark.parametrize("sex, eid_other, name, expected_eid", [
     (Sex.Sloubi, 84001, "Pipon", "18400108"),
